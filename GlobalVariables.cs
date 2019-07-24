5c4dfd783a36cb667b9dbcde7931921e385825ae
Rawat Amardeep ArjunPrasad <47588491+Amardeep9@users.noreply.github.com>
2019-07-24 13:06:07 +0530

    Add files via upload

---
+++ GlobalVariables.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace Mvc
+{
+    public static class GlobalVariables
+    {
+        public static HttpClient WebApiClient = new HttpClient();
+
+        static GlobalVariables()//Constructor
+        {
+            WebApiClient.BaseAddress = new Uri("http://localhost:65350/api/"); //Get it prom Properties of WebApi Project
+            WebApiClient.DefaultRequestHeaders.Clear();//clearing previous Default RequestHeaders
+            WebApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));//Creating Default RequestHeaders
+        }
+    }
+}
